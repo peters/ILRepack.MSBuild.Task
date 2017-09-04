1d4599b0e7748c63df15d49b27d26913b3447be7
Juho Rutila <juho.rutila@sympa.fi>
2017-09-04 09:30:48 +0300

    bump to 1.1.0

--- NuGetBuild.cmd
+++ NuGetBuild.cmd
@@ -2,4 +2,4 @@
 setlocal
 
 tools\NuGet.exe update -Self 
-tools\NuGet.exe pack ILRepack.MSBuild.Task.nuspec -OutputDirectory D:\Bruker_data\PeterLocal.NuGetRepository
+tools\NuGet.exe pack ILRepack.MSBuild.Task.nuspec -OutputDirectory Build
