e8176f4725f67572bafa7d7b7e9034bcb9318369
Peter Rekdal Sunde <peters@users.noreply.github.com>
2019-01-12 16:58:01 +0100

    Improve logger.

--- src/ILRepack.MSBuild.Task/ILRepackLogger.cs
+++ src/ILRepack.MSBuild.Task/ILRepackLogger.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Build.Framework;
+using Mono.Cecil;
 
 namespace ILRepack.MSBuild.Task
 {
@@ -18,32 +19,54 @@ namespace ILRepack.MSBuild.Task
 
         public void Log(object str)
         {
-            _task.Log.LogMessage(MessageImportance.High, str as string, str);
+            var message = str as string;
+            _task.Log.LogMessage(MessageImportance.High, $"{nameof(ILRepack)}: {message}", str);
         }
 
         public void Error(string msg)
         {
-            _task.Log.LogError(msg);
+            _task.Log.LogError($"{nameof(ILRepack)}: {msg}");
         }
 
         public void Warn(string msg)
         {
-            _task.Log.LogWarning(msg);
+            _task.Log.LogWarning($"{nameof(ILRepack)}: {msg}");
         }
 
         public void Info(string msg)
         {
-            _task.Log.LogMessage(MessageImportance.High, msg);
+            _task.Log.LogMessage(MessageImportance.High, $"{nameof(ILRepack)}: {msg}");
         }
 
         public void Verbose(string msg)
         {
-            _task.Log.LogMessage(MessageImportance.High, msg);
+            _task.Log.LogMessage(MessageImportance.High, $"{nameof(ILRepack)}: {msg}");
         }
 
         public void DuplicateIgnored(string ignoredType, object ignoredObject)
         {
-           
+            string type;
+  
+            switch (ignoredObject)
+            {
+                case FieldDefinition _:
+                    type = nameof(FieldDefinition);
+                    break;
+                case EventDefinition _:
+                    type = nameof(EventDefinition);
+                    break;
+                case PropertyDefinition _:
+                    type = nameof(PropertyDefinition);
+                    break;
+                case MethodDefinition _:
+                    type = nameof(MethodDefinition);
+                    break;
+                default:
+                    type = $"{ignoredObject?.GetType().Name}";
+                    break;
+            }
+
+           _task.Log.LogWarning($"{nameof(ILRepack)}: Duplicate ignored: {ignoredType}. Mono.Cecil type: {type}.");
         }
 
         public bool ShouldLogVerbose { get; set; }
