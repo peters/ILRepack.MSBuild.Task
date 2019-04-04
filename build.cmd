beb830108094bce7db60a3b23579e8289deac870
Oded Hanson <odhanson@microsoft.com>
2019-04-04 06:53:25 -0700

    Added an option to control the build configration from command line. This allows to easily create a debug build for the task

--- build.cmd
+++ build.cmd
@@ -3,13 +3,17 @@ setlocal
 
 set /p version="Please enter nuget release version: "
 
-call buildilrepackexe.cmd
+if not "%1"=="" SET CONFIG=%1
+if "%CONFIG%"=="" SET CONFIG=Release
 
-dotnet clean -c Release deps/ilrepack/ilrepack 
-dotnet build -c Release deps/ilrepack/ilrepack/ilrepack.csproj /p:ILRepackPackForRelease=true
-dotnet pack -c Release --no-build deps/ilrepack/ilrepack/ilrepack.csproj /p:Version=%version%
 
-dotnet clean -c Release deps/ilrepack/ilrepack
-dotnet clean -c Release src/ilrepack.msbuild.task 
-dotnet build -c Release src/ilrepack.msbuild.task/ilrepack.msbuild.task.csproj /p:ILRepackMSBuildTaskPackageForRelease=true
-dotnet pack -c Release --no-build src/ilrepack.msbuild.task/ilrepack.msbuild.task.csproj /p:ILRepackMSBuildTaskPackageForRelease=true /p:Version=%version%
+call buildilrepackexe.cmd %CONFIG%
+
+dotnet clean -c %CONFIG% deps/ilrepack/ilrepack 
+dotnet build -c %CONFIG% deps/ilrepack/ilrepack/ilrepack.csproj /p:ILRepackPackForRelease=true
+dotnet pack -c %CONFIG% --no-build deps/ilrepack/ilrepack/ilrepack.csproj /p:Version=%version%
+
+dotnet clean -c %CONFIG% deps/ilrepack/ilrepack
+dotnet clean -c %CONFIG% src/ilrepack.msbuild.task 
+dotnet build -c %CONFIG% src/ilrepack.msbuild.task/ilrepack.msbuild.task.csproj /p:ILRepackMSBuildTaskPackageForRelease=true
+dotnet pack -c %CONFIG% --no-build src/ilrepack.msbuild.task/ilrepack.msbuild.task.csproj /p:ILRepackMSBuildTaskPackageForRelease=true /p:Version=%version%
--- buildilrepackexe.cmd
+++ buildilrepackexe.cmd
@@ -1,8 +1,12 @@
 @echo off
 setlocal
 
-dotnet clean -c Release deps\ilrepack\ilrepack
-dotnet build -c Release -f net40 deps\ilrepack\ilrepack\ilrepack.csproj --output ..\..\..\build\ilrepack\windows /p:ILRepackExe=true
+if not "%1"=="" SET CONFIG=%1
+if "%CONFIG%"=="" SET CONFIG=Release
+
+echo CONFIG: %CONFIG%
+dotnet clean -c %CONFIG% deps\ilrepack\ilrepack
+dotnet build -c %CONFIG% -f net40 deps\ilrepack\ilrepack\ilrepack.csproj --output ..\..\..\build\ilrepack\windows /p:ILRepackExe=true
 build\ilrepack\windows\ilrepack.exe /target:exe /internalize build\ilrepack\windows\ilrepack.exe build\ilrepack\windows\bamlparser.dll build\ilrepack\windows\fasterflect.netstandard.dll build\ilrepack\windows\mono.cecil.dll /out:./build\ilrepack\windows\ilrepacked.exe
 copy build\ilrepack\windows\ilrepacked.exe build\ilrepack\windows\ilrepack.exe /b /Y 1>nul
 del /s /f /q build\ilrepack\windows\ilrepacked.exe 1>nul
